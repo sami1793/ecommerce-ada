315e9e3a3cb4e54e28b547ac6a22b8eb3f68ffe3
sami1793 <sami_17_94@hotmail.com>
2023-06-08 22:42:10 -0300

    Mejora de formulario register

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,14 +1,17 @@
 import {
+  Box,
   Button,
-  Container,
+  Center,
   Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
+  Heading,
+  IconButton,
   Input,
-  SimpleGrid,
   useToast,
 } from '@chakra-ui/react'
+import { ArrowBackIcon } from '@chakra-ui/icons'
 import { useForm } from 'react-hook-form'
 import { registerWithEmail } from '../../services/auth'
 import { useNavigate } from 'react-router-dom'
@@ -17,6 +20,10 @@ export const Register = () => {
   const { register, handleSubmit, formState } = useForm()
   const toast = useToast()
   const navigate = useNavigate()
+
+  const handleBackClick = () => {
+    navigate(-1) // Volver atrás en el historial de navegación
+  }
 
   const createAccount = async (data) => {
     try {
@@ -43,10 +50,86 @@ export const Register = () => {
   console.log(errors)
 
   return (
+    // <Flex bgColor="black.900" minH="100vh">
+    //   <Container as="form" onSubmit={handleSubmit(createAccount)}>
+    //     <SimpleGrid gap={5} justifyContent="center">
+    //       <FormControl isInvalid={errors.email}>
+    //         <FormLabel>Email</FormLabel>
+    //         <Input
+    //           type="email"
+    //           id="email"
+    //           {...register('email', {
+    //             required: {
+    //               value: true,
+    //               message: 'Este campo es requerido',
+    //             },
+    //             pattern: {
+    //               value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    //               message: 'Este email no es válido',
+    //             },
+    //           })}
+    //         />
+    //         <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+    //       </FormControl>
+    //       <FormControl isInvalid={errors.password}>
+    //         <FormLabel>Contraseña</FormLabel>
+    //         <Input
+    //           type="password"
+    //           id="password"
+    //           {...register('password', {
+    //             required: {
+    //               value: true,
+    //               message: 'Este campo es requerido',
+    //             },
+    //             minLength: {
+    //               value: 8,
+    //               message: 'El minimo de caracteres es 8',
+    //             },
+    //           })}
+    //         />
+    //         <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+    //       </FormControl>
+
+    //       <Button
+    //         mt={4}
+    //         colorScheme="teal"
+    //         type="submit"
+    //         isLoading={isSubmitting}
+    //       >
+    //         Crear cuenta
+    //       </Button>
+    //     </SimpleGrid>
+    //   </Container>
+    // </Flex>
     <Flex bgColor="black.900" minH="100vh">
-      <Container as="form" onSubmit={handleSubmit(createAccount)}>
-        <SimpleGrid gap={5} justifyContent="center">
-          <FormControl isInvalid={errors.email}>
+      <Box
+        mx="auto"
+        minW={{ base: '95%', sm: '400px' }}
+        mt={8}
+        mb={8}
+        p={6}
+        borderWidth="1px"
+        borderRadius="md"
+        boxShadow="md"
+        bgColor="white"
+      >
+        <IconButton
+          icon={<ArrowBackIcon />}
+          aria-label="Volver atrás"
+          mb={4}
+          variant="solid"
+          color="white"
+          bg="black.900"
+          border="2px"
+          _hover={{ bg: 'black', color: 'white' }}
+          onClick={() => handleBackClick()}
+        />
+        <Heading as="h2" size="lg" textAlign="center" mb={6}>
+          Registrarse
+        </Heading>
+
+        <form onSubmit={handleSubmit(createAccount)}>
+          <FormControl id="email" isInvalid={errors.email} isRequired>
             <FormLabel>Email</FormLabel>
             <Input
               type="email"
@@ -64,7 +147,12 @@ export const Register = () => {
             />
             <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
           </FormControl>
-          <FormControl isInvalid={errors.password}>
+          <FormControl
+            id="password"
+            mt={4}
+            isInvalid={errors.password}
+            isRequired
+          >
             <FormLabel>Contraseña</FormLabel>
             <Input
               type="password"
@@ -82,17 +170,22 @@ export const Register = () => {
             />
             <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
           </FormControl>
-
-          <Button
-            mt={4}
-            colorScheme="teal"
-            type="submit"
-            isLoading={isSubmitting}
-          >
-            Crear cuenta
-          </Button>
-        </SimpleGrid>
-      </Container>
+          <Center>
+            <Button
+              type="submit"
+              bg="black"
+              border="2px"
+              color="white"
+              _hover={{ bg: 'white', color: 'black' }}
+              mt={6}
+              isFullWidth
+              isLoading={isSubmitting}
+            >
+              Crear cuenta
+            </Button>
+          </Center>
+        </form>
+      </Box>
     </Flex>
   )
 }
